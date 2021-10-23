37b420160f86de3bd82cbb6f35311fe2407bca62
nurpeiis <nb2577@nyu.edu>
2021-10-22 20:08:30 -0400

    Finish 2nd lesson

---
+++ freeCodeCamp/lesson_2/SimpleStorageFactory.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "../lesson_1/SimpleStorage.sol";
+
+/*
+1. Use other smart contracts inside your contract
+2. Inheritance
+3. Use methods in other smart contracts inside your contract
+*/
+
+contract StorageFactory is SimpleStorage {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        //address
+        //ABI
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(
+            _simpleStorageNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return
+            SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
+                .retrieve();
+    }
+}
